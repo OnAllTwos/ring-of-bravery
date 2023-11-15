1fcac464e3d740a6c49f88f225c93ab1dbc9bfc7
OnAllTwos <gamebuilder116@gmail.com>
2023-11-14 20:15:13 -0800

    Began writing database interface, currently nonfunctional

--- src/api/ring_of_bravery_api/interfaces/synergy_database.py
+++ src/api/ring_of_bravery_api/interfaces/synergy_database.py
@@ -1,1 +1,16 @@
 # TODO: Create a nice interface for grabbing stuff from the database here. Use SQLModel.
+from ring_of_bravery_api.types.data_types import Augments
+from sqlmodel import create_engine, Session
+
+class SynergyDatabase:
+
+    def __init__(self, base_url: str) -> None:
+        self.base_url = base_url
+        self.engine = create_engine(base_url, echo=True)
+        self.engine.connect()
+    
+    def test(self) -> None:
+        with Session(self.engine) as session:
+            test = Augments(name="test")
+            session.add(test)
+            session.commit()
